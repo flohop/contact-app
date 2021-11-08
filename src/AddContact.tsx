1adda24edf50c5f441ea0d224e4f531fef7128dd
flohop <hoppe.florian02@gmail.com>
2021-11-08 12:42:05 +0100

    added add contact page

--- src/AddContact.tsx
+++ src/AddContact.tsx
@@ -1,8 +1,103 @@
-import React from "react";
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  useToast,
+  Box,
+  Center,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import * as EmailValidator from "email-validator";
 
 type AddContactProps = {};
 
 const AddContact: React.FC<AddContactProps> = ({}) => {
-  return <p>Add contact</p>;
+  const [newFirstName, setNewFirstName] = useState("");
+  const [newLastName, setNewLastName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+
+  const toast = useToast();
+
+  const submitForm = async () => {
+    // make sure the input is valid
+    if (
+      newFirstName.length === 0 ||
+      newLastName.length === 0 ||
+      !EmailValidator.validate(newEmail)
+    ) {
+      // show error text
+      toast({
+        title: "Invalid input",
+        description: "Please fill out the form",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      // form is valid
+      // form is valid
+      // add document
+      toast({
+        title: "Added contact",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Center>
+      <Box width={"70%"}>
+        <FormControl
+          id="first-name"
+          isRequired
+          isInvalid={newFirstName.length === 0}
+        >
+          <FormLabel>First Name</FormLabel>
+          <Input
+            type="text"
+            value={newFirstName}
+            onChange={(e) => setNewFirstName(e.target.value)}
+          />
+        </FormControl>
+        <FormControl
+          id="last-name"
+          isRequired
+          isInvalid={newLastName.length === 0}
+        >
+          <FormLabel>Last Name</FormLabel>
+          <Input
+            type="text"
+            value={newLastName}
+            onChange={(e) => setNewLastName(e.target.value)}
+          />
+        </FormControl>
+        <FormControl
+          id="email"
+          isRequired
+          isInvalid={!EmailValidator.validate(newEmail)}
+        >
+          <FormLabel>Email address</FormLabel>
+          <Input
+            type="email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+          />
+        </FormControl>
+        <Button
+          mt={"5"}
+          leftIcon={<AddIcon />}
+          colorScheme="teal"
+          variant="solid"
+          onClick={() => submitForm()}
+        >
+          Add Contact
+        </Button>
+      </Box>
+    </Center>
+  );
 };
 export default AddContact;
