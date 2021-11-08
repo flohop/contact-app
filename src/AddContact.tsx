2f4abe1fc88dd788b10ac1064c9ff598e8c4805e
flohop <hoppe.florian02@gmail.com>
2021-11-08 12:36:07 +0100

    added chakraUi, router and added basic paths

---
+++ src/AddContact.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type AddContactProps = {};
+
+const AddContact: React.FC<AddContactProps> = ({}) => {
+  return <p>Add contact</p>;
+};
+export default AddContact;
---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type HomeProps = {};
+
+const Home: React.FC<HomeProps> = ({}) => {
+  return <p>Home</p>;
+};
+export default Home;
